7fbab046ac353f646a639b90d32bd87c49836cf9
Aqniet5 <mudaigaliakniet@gmail.com>
2026-01-25 15:37:25 +0500

    practice

---
+++ practice/fifth.py
@@ -0,0 +1,17 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+xx = 1
+yy = 35656222554887711
+zz = -3255522
+
+print(type(xx))
+print(type(yy))
+print(type(zz))
+xxx = 35e3
+yyy = 12E4
+zzz = -87.7e100
+
+print(type(xxx))
+print(type(yyy))
+print(type(zzz))
